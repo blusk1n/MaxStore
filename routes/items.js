ebb6714088488c18099e6c36a729ac26f409fa7f
Omar Bara <omarbara@ymail.com>
2020-01-07 16:03:34 +0100

    items

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,5 @@
 const router = module.exports = require('express').Router()
+const Item = require('../model/item.js')
 
 
 router.get('/', function(req, res){
@@ -6,7 +7,25 @@ router.get('/', function(req, res){
 })
 
 router.post('/', function(req, res){
-    res.json({"masd":"asda"})
+    
+    var data = { 
+        "name":  req.body.name, 
+        "description":req.body.description, 
+        "photo":req.body.photo, 
+        "user":req.body.user,
+        "available":req.body.available,
+        "quantity":req.body.quantity, 
+        "deactivated":req.body.deactivated, 
+        "category":req.body.category
+    }
+
+    Item.create(data, function (err, items) {
+        if (err) {
+            return next(err)
+        } else {
+            return res.status(400).send(error.details[0].message)
+        }
+        }); 
 })
 
 router.patch('/', function(req, res){

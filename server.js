aebb65626085ab9bd2619eeaef3b9849b2b1d490
Moriarty <ahmedehsouna@yahoo.com>
2020-01-07 12:14:12 +0100

    starting with react

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express           = require("express"),
       bodyParser        = require("body-parser"),
       mongoose          = require("mongoose"),
       app               = express(),
+      path              = require("path"),
       port              = process.env.PORT || 3000
 
     
 
 
 app.use(express.static(__dirname + "/public"))
+app.get("*" , (req,res)=>res.sendFile(path.resolve("public" , "index.html")))
 app.listen(port)
 

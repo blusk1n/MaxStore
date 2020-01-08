c9c8111455000259859308faaf1ccc02b50e09b2
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-01-08 16:22:20 +0100

    Done with the review and the rate routes

--- routes/items.js
+++ routes/items.js
@@ -1,11 +1,57 @@
 const router = module.exports = require('express').Router()
 const Item = require('../model/item.js')
-const mongoose = require('mongoose');
+const Rate = require('../model/rate.js')
+const Review = require('../model/review.js')
 
 router.patch('/:id', function (req, res) {
 
     Item.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, items) {
-        console.log(items)
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(items)
+        }
+    });
+})
+
+router.post('/:id/reviews', function (req, res) {
+    let object = { item: req.params.id, user: req.user._id, content: req.body.content };
+
+    Review.create(object, function (err, items) {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(items)
+        }
+    });
+})
+
+router.post('/:id/rates', function (req, res) {
+    let object = { item: req.params.id, user: req.user._id, stars: req.body.stars };
+
+    Rate.create(object, function (err, items) {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(items)
+        }
+    });
+})
+
+router.get('/:id/rates', function (req, res) {
+
+    Rate.find({ item: req.params.id }, function (err, items) {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.send(items)
+        }
+    });
+})
+
+router.get('/:id/reviews', function (req, res) {
+
+    Review.find({ item: req.params.id }, function (err, items) {
         if (err) {
             return res.send(err)
         } else {

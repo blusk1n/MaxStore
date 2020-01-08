761e4710e9a3f8ddfe78698da6865c3e968c0423
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-01-08 12:17:21 +0100

    working on the items in the routes

--- routes/items.js
+++ routes/items.js
@@ -2,33 +2,33 @@ const router = module.exports = require('express').Router()
 const Item = require('../model/item.js')
 
 
-router.get('/', function(req, res){
-    res.json({"masd":"asda"})
+router.get('/', function (req, res) {
+    res.json({ "masd": "asda" })
 })
 
-router.post('/', function(req, res){
-    
-    var data = { 
-        "name":  req.body.name, 
-        "description":req.body.description, 
-        "photo":req.body.photo, 
-        "price":req.body.price,
-        "available":req.body.available,
-        "quantity":req.body.quantity, 
-        "deactivated":req.body.deactivated, 
-        "category":req.body.category
+router.post('/', function (req, res) {
+
+    var data = {
+        "name": req.body.name,
+        "description": req.body.description,
+        "photo": req.body.photo,
+        "price": req.body.price,
+        "available": req.body.available,
+        "quantity": req.body.quantity,
+        "deactivated": req.body.deactivated,
+        "category": req.body.category
     }
 
     Item.create(data, function (err, items) {
         if (err) {
-            return console.log(err)
+            return res.send(err)
         } else {
-            return res.status(400).send(error.details[0].message)
+            return res.send(items)
         }
-        }); 
-        res.status(200)
+    });
+    res.status(200)
 })
 
-router.patch('/', function(req, res){
-    res.json({"masd":"asda"})
+router.patch('/', function (req, res) {
+    res.json({ "masd": "asda" })
 })
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express           = require("express"),
-      bodyParser        = require("body-parser"),
-      mongoose          = require("mongoose"),
-      app               = express(),
-      users             = require('./routes/users.js'),
-      items             = require('./routes/items.js'),
-      path              = require("path"),
-      port              = process.env.PORT || 3000
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    app = express(),
+    users = require('./routes/users.js'),
+    items = require('./routes/items.js'),
+    path = require("path"),
+    port = process.env.PORT || 3000
 const passport = require('passport')
-    
 
-mongoose.connect('mongodb://maxst0re:maxst0re@ds149676.mlab.com:49676/maxstore', { useNewUrlParser: true , useUnifiedTopology: true } , ()=>console.log("database is working")) 
+
+// mongoose.connect('mongodb://maxst0re:maxst0re@ds149676.mlab.com:49676/maxstore', { useNewUrlParser: true , useUnifiedTopology: true } , ()=>console.log("database is working")) 
+mongoose.connect('mongodb://localhost/Maxshop', { useUnifiedTopology: true }, () => console.log("database is working"));
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -23,7 +24,7 @@ require('./config/passport')(passport)
 
 app.use('/api/users', users)
 app.use('/api/items', items)
-app.get("*" , (req,res)=>res.sendFile(path.resolve("public" , "index.html")))
+app.get("*", (req, res) => res.sendFile(path.resolve("public", "index.html")))
 app.listen(port, () => console.log(`app running on port ${port}`))
 
 
